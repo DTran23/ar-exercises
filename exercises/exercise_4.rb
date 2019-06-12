f7f1f57f6264e5ac30f18138dbe49928d90e42c1
Daniel <nhanxtrand@gmail.com>
2019-06-12 08:50:55 +0000

    Completed AR ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,23 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+puts "Mens Apparel Stores"
+puts "-------------------"
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "Store name: #{store[:name]}, Annual Revenue: #{store[:annual_revenue]}"
+end
+
+puts "Womens Apparel Stores"
+puts "-------------------"
+
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.each do |store|
+  puts "Store name: #{store[:name]}, Annual Revenue: #{store[:annual_revenue]}"
+end
